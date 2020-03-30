c897fee4b00cd5a87a8bd1f2c84ac68ca30ce608
Jakob Jordan <jakobjordan@posteo.de>
2020-03-30 18:06:59 +0200

    Fix NEAT installation
    - pre-netsim install hook needs to be in build_ext
    - compilechannels can not be called during installation as NEAT is not
    installed yet; needs to be called separately after installation
    - mod files are specified directly and copied by setup to the correct destination

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ Author: W. Wybo
 
 from setuptools import setup
 from setuptools.extension import Extension
-from setuptools.command.develop import develop
-from setuptools.command.install import install
+from setuptools.command.build_ext import build_ext
 
 import os, subprocess, shutil, sys
 
@@ -18,33 +17,14 @@ import numpy
 from __version__ import version as pversion
 
 
-"""
-Define pre- and post-install commands via command classes.
-From https://stackoverflow.com/questions/20288711/post-install-script-with-python-setuptools
-"""
+class BuildExtCommand(build_ext):
 
-class PostDevelopCommand(develop):
-    """Post-installation for development mode."""
     def run(self):
-        # execute pre installation commands
+        # execute pre build ext commands
         write_ionchannel_header_and_cpp_file()
-        copy_default_neuron_mechanisms()
-        # develop install
-        develop.run(self)
-        # execute post installation commands
-        compile_default_ion_channels()
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        # execute pre installation commands
-        write_ionchannel_header_and_cpp_file()
-        copy_default_neuron_mechanisms()
-        # regular install
-        install.run(self)
-        # execute post installation commands
-        compile_default_ion_channels()
+        # build ext
+        build_ext.run(self)
+        # execute post build ext commands
 
 
 def write_ionchannel_header_and_cpp_file():
@@ -53,41 +33,10 @@ def write_ionchannel_header_and_cpp_file():
     the netsim.pyx extension.
 
     """
-    # we can import even before installation as we are in the root
+    # we can import before installation as we are in the root
     # directory
     import neat.channels.writecppcode as writecppcode
     writecppcode.write()
-
-
-def copy_default_neuron_mechanisms():
-    """
-    Copies the default neuron mechanisms from
-        neat/tools/simtools/neuron/mech_storage/
-    to
-        neat/tools/simtools/neuron/mech/
-    and creates an `__init__.py` file in that directory.
-
-    The mech/ directory is generated in a clean state by the installer and
-    aggregates all neuron .mod files. Hence, it should never be used for
-    permanent storage.
-
-    """
-    mech_path = 'neat/tools/simtools/neuron/mech/'
-    if os.path.exists(mech_path):
-        shutil.rmtree(mech_path)
-    shutil.copytree('neat/tools/simtools/neuron/mech_storage/', mech_path)
-    open('neat/tools/simtools/neuron/mech/__init__.py', 'w').close()
-
-
-def compile_default_ion_channels():
-    """
-    Compiles the default ion channels found in channelcollection for
-    use with NEURON.
-    """
-    cwd = os.getcwd()
-    os.chdir('neat/channels/')
-    subprocess.call(['./compilechannels', 'channelcollection/'])
-    os.chdir(cwd)
 
 
 def read_requirements():
@@ -117,16 +66,16 @@ s_ = setup(
               'neat.tools.fittools',
               'neat.tools.plottools',
               'neat.tools.simtools.neuron',
-              'neat.tools.simtools.neuron.mech',
               'neat.channels',
               'neat.channels.channelcollection'],
+    package_data={
+        "neat.tools.simtools.neuron": ["mech/*.mod"],
+    },
     ext_package='neat',
     ext_modules=[ext],
     cmdclass={
-        'develop': PostDevelopCommand,
-        'install': PostInstallCommand,
+        'build_ext': BuildExtCommand,
     },
-    include_package_data=True,
     author='Willem Wybo',
     classifiers=['Development Status :: 3 - Alpha',
                  'Programming Language :: Python :: 3.7'],
